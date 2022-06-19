7b8a052c4a7ae5fce6386466ab65d7471608277a
Sergio G Cosgaya <sergiogcosgaya@gmail.com>
2022-06-19 13:36:45 +0200

    chore: Test build without minifying

---
+++ packages/utils/build.js
@@ -0,0 +1,17 @@
+/* eslint-disable eslint-comments/disable-enable-pair */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { buildPackage } = require('@sergiogc9/js-bundle');
+
+const isWatchMode = process.argv.includes('--watch');
+
+const performBuild = async () => {
+	await buildPackage({
+		isWatchMode,
+		esbuildOptions: {
+			minify: false
+		}
+	});
+};
+
+performBuild();
